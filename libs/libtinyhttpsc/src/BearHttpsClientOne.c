5ce08fdf8f0bf01545368e7a0127dee557eb1da1
misazhu <misa.zhu@gmail.com>
2026-03-19 22:05:22 +0800

    httpsc dns resolv

--- libs/libtinyhttpsc/src/BearHttpsClientOne.c
+++ libs/libtinyhttpsc/src/BearHttpsClientOne.c
@@ -21195,16 +21195,14 @@ void BearHttpsRequest_free(BearHttpsRequest *self);
 #if !defined(__EMSCRIPTEN__)
 
 
-
-BearHttpsClientDnsProvider privateBearHttpsProviders[] = {
+/*BearHttpsClientDnsProvider privateBearHttpsProviders[] = {
     {.hostname= "dns.google.com",.route="/resolve",.ip = "8.8.8.8",.port=443},
     {.hostname= "dns.nextdns.io",.route="/dns-query",.ip = "217.146.9.93",.port=443},
 
    
 };
-
-
 int privateBearHttpsProvidersSize = sizeof(privateBearHttpsProviders)/sizeof(BearHttpsClientDnsProvider);
+*/
 
 
 #endif
