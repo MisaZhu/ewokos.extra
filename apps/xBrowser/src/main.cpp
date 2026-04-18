7d4dd4a4026bbdc6c1cf588fc3982fe27081b778
misazhu <misa.zhu@gmail.com>
2026-04-18 23:28:52 +0800

    WidgetWebview

--- apps/xBrowser/src/main.cpp
+++ apps/xBrowser/src/main.cpp
@@ -1,65 +1,42 @@
-// Main entry point using xwin C API
+// Main entry point using widget++
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <ewoksys/proc.h>
-#include <x/x.h>
-#include <x/xwin.h>
-#include <graph/graph.h>
-#include "XBrowserWnd.h"
+#include <x++/X.h>
+#include <Widget/WidgetWin.h>
+#include <Widget/RootWidget.h>
+#include <Widget/WidgetX.h>
+#include <Widget/EditLine.h>
 
-static XBrowserWnd* g_browser = NULL;
+#include "WidgetWebview/WidgetWebview.h"
 
-static void on_repaint(xwin_t* xwin, graph_t* g)
-{
-    (void)xwin;
-    if (g_browser && g) {
-        g_browser->repaint(g);
-    }
-}
-
-static void on_resize(xwin_t* xwin)
-{
-    (void)xwin;
-    if (g_browser && xwin && xwin->xinfo) {
-        g_browser->handleWindowResize(xwin->xinfo->winr.w, xwin->xinfo->winr.h);
-    }
-}
-
-static void loop(void* p)
-{
-    xwin_t* xwin = (xwin_t*)p;
-    xwin_repaint(xwin);
-    proc_usleep(16000);
-}
+using namespace Ewok;
 
 int main(int argc, char* argv[])
 {
-    (void)argc;
-    (void)argv;
+    X x;
+    WidgetWin win;
+    RootWidget* root = new RootWidget();
+    win.setRoot(root);
+    root->setType(Container::VERTICAL);
 
-    x_t x;
-    x_init(&x, NULL);
+    EditLine* editline = new EditLine();
+    root->add(editline);
+    editline->fix(0, 24);
+    root->focus(editline);
 
-    xwin_t* xwin = xwin_open(&x, -1, 32, 32, 640, 480, "HTML Browser", XWIN_STYLE_NORMAL);
-    if (!xwin) {
-        printf("Failed to open window\n");
-        return 1;
+    WidgetWebview* webview = new WidgetWebview();
+    root->add(webview);
+
+    if(argc > 1) {
+        editline->disable();
+        editline->setContent(argv[1]);
+        webview->load(argv[1]);
+    }
+    else {
+        editline->enable();
     }
 
-    g_browser = new XBrowserWnd();
-    g_browser->setXWin(xwin);
-    g_browser->loadMedia();
+    win.open(&x, -1, -1, -1, 0, 0, "HTML Browser", XWIN_STYLE_NORMAL);
+    widgetXRun(&x, &win);
 
-    xwin->on_repaint = on_repaint;
-    xwin->on_resize = on_resize;
-    x.on_loop = loop;
-
-    xwin_set_visible(xwin, true);
-
-    x_run(&x, xwin);
-
-    delete g_browser;
     return 0;
 }
