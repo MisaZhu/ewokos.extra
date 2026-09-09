fda2ea9152e41c74b756542d0ca350e96eaf9be4
misazhu <misa.zhu@gmail.com>
2026-09-09 20:45:03 +0800

    vim v-cmd

--- bin/evim/vim_text.c
+++ bin/evim/vim_text.c
@@ -884,7 +884,7 @@ int file_write(char* fn, char* first, char* last) {
     // but instead ftruncate() it _after_ successful write.
     // Might reduce amount of data lost on power fail etc.
     //fd = open(fn, O_WRONLY | O_CREAT | O_TRUNC);
-    fd = open(fn, O_RDWR | O_CREAT | O_TRUNC);
+    fd = open(fn, O_RDWR | O_CREAT | O_TRUNC, 0666);
     if(fd < 0)
         return -1;
     cnt = last - first + 1;
